13b19ab07d427a4f0aa81b6e7a91edec402f0db0
NIRAV <ndesai187@users.noreply.github.com>
2016-10-01 12:43:55 +0800

    Android Tutorial 1

---
+++ app/src/main/java/com/example/nirav/pangrams_v1/CheckPangramsActivity.java
@@ -0,0 +1,17 @@
+package com.example.nirav.pangrams_v1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class CheckPangramsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_pangrams);
+        Pangrams pg = new Pangrams();
+        pg.userInput();
+        Toast.makeText(this, "hijo de puta", Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/com/example/nirav/pangrams_v1/Pangrams.java
@@ -0,0 +1,22 @@
+package com.example.nirav.pangrams_v1;
+
+import java.util.Scanner;
+
+/**
+ * Created by Nirav on 28/09/16.
+ */
+public class Pangrams {
+
+    //private Scanner input = new Scanner(System.in);
+
+    public void userInput(){
+        String a = "Hello";
+
+        System.out.println("Input String : ");
+       // a = input.nextLine();
+
+        System.out.println("This is your String : " + a);
+
+    }
+
+}

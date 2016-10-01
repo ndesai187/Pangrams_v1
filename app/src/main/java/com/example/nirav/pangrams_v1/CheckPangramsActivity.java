e61eea7e615e3151988d1abd1bf67ca3039b037a
NIRAV <ndesai187@users.noreply.github.com>
2016-10-02 00:44:19 +0800

    Created app for user input test

--- app/src/main/java/com/example/nirav/pangrams_v1/CheckPangramsActivity.java
+++ app/src/main/java/com/example/nirav/pangrams_v1/CheckPangramsActivity.java
@@ -2,17 +2,54 @@ package com.example.nirav.pangrams_v1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class CheckPangramsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_check_pangrams);
-        Pangrams pg = new Pangrams();
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_check_pangrams );
+
+        final Button b1 = (Button) findViewById(R.id.ClickHere );
+        b1.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText( CheckPangramsActivity.this, "hijo de puta", Toast.LENGTH_LONG ).show();
+            }
+        } );
+
+    /*Pangrams pg = new Pangrams();
         pg.userInput();
-        Toast.makeText(this, "hijo de puta", Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"hijo de puta",Toast.LENGTH_LONG).show();*/
+
+
+}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate( R.menu.check_pangrams, menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected( item );
     }
 
     //dummy for commit
